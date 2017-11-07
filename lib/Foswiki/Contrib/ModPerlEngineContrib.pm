e2a273a256b6a41b77662c61e2529a6ef5712e24
Stephan Osthold <osthold@modell-aachen.de>
2017-11-07 12:25:32 +0100

    ID:3802 Remove SVN version string
    Ticket: none
    Release-Note: none

--- lib/Foswiki/Contrib/ModPerlEngineContrib.pm
+++ lib/Foswiki/Contrib/ModPerlEngineContrib.pm
@@ -23,7 +23,7 @@ use strict;
 
 use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION );
 
-$VERSION          = '$Rev$';
+$VERSION          = '0.9';
 $RELEASE = "0.9";
 $SHORTDESCRIPTION = 'Permits Foswiki to be executed under mod_perl';
 
